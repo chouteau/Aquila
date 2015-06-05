fdc3648769e6715ae00f19df2ed399016be83e5f
Marc Chouteau <chouteau@gmail.com>
2015-06-05 15:11:56 +0200

    Cleanup

---
+++ src/Aquila/ProductCustomMetric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquila
+{
+	public class ProductCustomMetric
+	{
+		/// <summary>
+		/// A product-level custom metric where metric index is a positive integer between 1 and 200, inclusive. Product index must be a positive integer between 1 and 200, inclusive. For analytics.js the Enhanced Ecommerce plugin must be installed before using this field.
+		/// </summary>
+		/// <example>
+		/// Example value: 28
+		/// Example usage: pr<productIndex>cm<metricIndex>=28
+		/// </example>
+		[Parameter(@"pr<index>cm<dindex>")]
+		public int Metric { get; set; }
+
+	}
+}
