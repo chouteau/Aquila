cc7c79b58f9d99bd8c980d36e9f4302f0103a2ee
Marc Chouteau <chouteau@gmail.com>
2017-02-11 16:18:30 +0100

    - Remove dependency of Microsoft.Web.Infrastructure - Separate assembly need IISModule to Aquila.IISModule (1.0.0.0)

--- src/Aquila.IISModule/Properties/AssemblyInfo.cs
+++ src/Aquila.IISModule/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aquila")]
+[assembly: AssemblyTitle("Aquila.IISModule")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aquila")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Aquila.IISModule")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("959a9659-88c7-4061-bf95-d10d36322b41")]
+[assembly: Guid("f128dc15-b2be-4084-b989-b5d6b95d89d1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.7.15.0")]
-[assembly: AssemblyFileVersion("3.7.15.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
